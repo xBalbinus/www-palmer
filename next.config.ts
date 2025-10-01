36b69ff0aee23befacb1eb1031cc8be78baa2fdf
xBalbinus <xiangan9@gmail.com>
2025-10-01 13:14:38 -0400

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: undefined,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
